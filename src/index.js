f6343295e6fb327716e7ec29398025a4a4e3d312
FarBusC4 <marcosfer243@gmail.com>
2022-05-14 22:44:29 -0300

    Basename 2

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { store } from "./store/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="go-scrum/">
+    <BrowserRouter basename="Go-scrum/">
       <Provider store={store}>
         <App />
       </Provider>
